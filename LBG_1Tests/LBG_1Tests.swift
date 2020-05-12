e1a3a574716ca13a734d8a22c3e2a1406f6107b2
Satyaranjan Mohapatra <satyaranjanmohapatra@rocketmail.com>
2020-05-12 21:37:58 +0530

    Initial Commit

---
+++ LBG_1Tests/LBG_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  LBG_1Tests.swift
+//  LBG_1Tests
+//
+//  Created by SatyaRanjan Mohapatra on 12/05/20.
+//  Copyright © 2020 SatyaranjanMohapatra. All rights reserved.
+//
+
+import XCTest
+@testable import LBG_1
+
+class LBG_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
